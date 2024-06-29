e054a50d049662bc88fd29167a7904130ef48890
s24729-pj <s24729@pjwstk.edu.pl>
2024-06-29 10:12:20 +0200

    Extracted browser playlist into a separate component

--- src/components/Browser/Browser.jsx
+++ src/components/Browser/Browser.jsx
@@ -1,20 +1,10 @@
 import styles from './Browser.module.css';
 import {BiLibrary} from "react-icons/bi";
 import {IoMdAdd} from "react-icons/io";
+import BrowserPlaylist from "../BrowserPlaylist/BrowserPlaylist.jsx";
 
 
 function Browser({playlists}) {
-  function trimString(str) {
-    const words = str.split(' ');
-    const trimmed = words.slice(0, 4);
-
-    return trimmed.join(' ');
-  }
-
-  function onPlaylistClick() {
-    console.log("onPlaylistClick");
-  }
-
   return (
     <nav className={styles.browser}>
       <header className={styles.browserHeader}>
@@ -26,14 +16,7 @@ function Browser({playlists}) {
       </header>
       <div className={styles.browserContent}>
         {playlists.map((playlist, index) => (
-          <div key={index} className={styles.playlist} onClick={onPlaylistClick}>
-            <img className={styles.playlistCoverArt} src={playlist.coverArtUrl}
-                 alt={`Playlist ${playlist.title} cover art`}/>
-            <div className={styles.playlistInfo}>
-              <h3 className={styles.playlistTitle}>{trimString(playlist.title)}</h3>
-              <p className={styles.playlistAuthor}>{`Playlist - ${playlist.author}`}</p>
-            </div>
-          </div>
+          <BrowserPlaylist key={index} playlist={playlist}/>
         ))}
       </div>
     </nav>
---
+++ src/components/BrowserPlaylist/BrowserPlaylist.jsx
@@ -0,0 +1,23 @@
+import styles from './BrowserPlaylist.module.css'
+
+function BrowserPlaylist({playlist, onPlaylistClick}) {
+  function trimString(str) {
+    const words = str.split(' ');
+    const trimmed = words.slice(0, 4);
+
+    return trimmed.join(' ');
+  }
+
+  return (
+    <div className={styles.playlist} onClick={onPlaylistClick}>
+      <img className={styles.playlistCoverArt} src={playlist.coverArtUrl}
+           alt={`Playlist ${playlist.title} cover art`}/>
+      <div className={styles.playlistInfo}>
+        <h3 className={styles.playlistTitle}>{trimString(playlist.title)}</h3>
+        <p className={styles.playlistAuthor}>{`Playlist - ${playlist.author}`}</p>
+      </div>
+    </div>
+  );
+}
+
+export default BrowserPlaylist;
