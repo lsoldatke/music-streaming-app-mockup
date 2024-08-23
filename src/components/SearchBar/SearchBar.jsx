460bce1df8edee86b158c2d3f8c898be45a76788
lukas <s24729@pjwstk.edu.pl>
2024-08-23 15:19:37 +0200

    Works on search bar

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import {useState} from "react";
+
+function SearchBar() {
+  const [searchedPhrase, setSearchedPhrase] = useState('');
+
+  return (
+    <div>
+      <input/>
+      <button/>
+    </div>
+  );
+}
+
+export default SearchBar;
