cf3169a942f90aebdf17f671738bc65340c9961d
lukas <s24729@pjwstk.edu.pl>
2024-08-26 20:48:42 +0200

    Works on search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,16 @@
 import {useState} from "react";
+import styles from "./SearchBar.module.css";
+import {FaSearch} from "react-icons/fa";
 
 function SearchBar() {
+  const [isExpanded, setIsExpanded] = useState(false);
   const [searchedPhrase, setSearchedPhrase] = useState('');
 
   return (
-    <div>
-      <input/>
-      <button/>
+    <div className={styles.searchBar}>
+      <input className={styles.searchInput} type="text" onMouseEnter={() => setIsExpanded(true)}
+             onMouseLeave={() => setIsExpanded(false)} placeholder="Search for something..."/>
+      <button className={styles.searchButton}><FaSearch/></button>
     </div>
   );
 }
